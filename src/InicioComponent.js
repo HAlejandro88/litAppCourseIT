669c82fa210047c1a9c1563531045a696736a71f
Alejandro <alexsotog82@gmail.com>
2020-06-23 15:57:47 -0500

    complete

--- src/InicioComponent.js
+++ src/InicioComponent.js
@@ -1,8 +1,11 @@
 import { html, css, LitElement } from 'lit-element';
-import { PageViewElement } from './views/page-view-element'
+import { PageViewElement } from './views/page-view-element';
 import './views/view-home';
 import './views/view-about'; 
 import './views/view-contact';
+import 'dile-tabs/dile-tabs';
+import 'dile-tabs/dile-tab';
+import 'dile-pages/dile-pages';
 
 export class InicioComponent extends LitElement {
   static get styles() {
@@ -10,6 +13,9 @@ export class InicioComponent extends LitElement {
       :host {
         display: block;
         padding: 25px;
+        --dile-tab-background-color: transparent;
+        --dile-tab-selected-background-color: transparent;
+        --dile-tab-selected-text-color: #102080;
       }
       .page{
         display: none;
@@ -28,24 +34,34 @@ export class InicioComponent extends LitElement {
 
   constructor() {
     super();
-    this.selected = 'home';
+    // this.selected = 'home';
   }
 
 
   render() {
     return html`
       <h1>Mi componente</h1>
-      <nav>
+      <!-- <nav>
         <a href="#" @click="${this.go}" name="home">Home</a>
         <a href="#" @click="${this.go}" name="about">About</a>
         <a href="#" @click="${this.go}" name="contact">Contact</a>
-      </nav>
-      <main>
-        <view-home class="page" ?active=${this.selected == 'home'}></view-home>
-        <view-about class="page" ?active=${this.selected == 'about'}></view-about>
-        <view-contact class="page" ?active=${this.selected == 'contact'}></view-contact>
-      </main>
+      </nav> -->
+      <dile-tabs selected="${this.selected}" attrForSelected="name" @dile-tabs-selected-changed="${this.selectedChanged}">
+        <dile-tab name="home">Home</dile-tab>
+        <dile-tab name="about">About</dile-tab>
+        <dile-tab name="contact">Contact</dile-tab>
+      </dile-tabs>
+      <dile-pages selected="${this.selected}" attrForSelected="name">
+        <view-home name="home" ?active=${this.selected == 'home'}></view-home>
+        <view-about name="about" ?active=${this.selected == 'about'}></view-about>
+        <view-contact name="contact" ?active=${this.selected == 'contact'}></view-contact>
+      </dile-pages>
     `;
+  }
+
+  selectedChanged(e) {
+    console.log(e.detail);
+    this.selected = e.detail;
   }
 
   goHome(e) {
